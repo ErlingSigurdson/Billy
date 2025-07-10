d6c67e6bd9c8d15a30c9a3317dfe76913621d354
Erling Sigurdson <efimov-d-v@yandex.ru>
2025-07-11 02:19:45 +0300

    Add Drv7seg4d2x595 library.

---
+++ sketch/Billy/Drv7seg4d2x595.cpp
@@ -0,0 +1,103 @@
+/****************** DESCRIPTION *****************/
+
+/**
+ * Filename: Drv7seg4d2x595.cpp
+ * ----------------------------------------------------------------------------|---------------------------------------|
+ * Purpose:
+ * ----------------------------------------------------------------------------|---------------------------------------|
+ * Notes:
+ */
+
+
+/************ PREPROCESSOR DIRECTIVES ***********/
+
+/*--- Includes ---*/
+
+// Essential Arduino library.
+#include <Arduino.h>
+
+// Additional libraries for Arduino IDE.
+#include <SPI.h>
+
+// Local modules.
+#include "Drv7seg4d2x595.h"
+#include "config_general.h"
+
+
+/*************** GLOBAL VARIABLES ***************/
+
+Drv7seg4d2x595 Daisy16 = Drv7seg4d2x595();
+
+
+/******************* FUNCTIONS ******************/
+
+Drv7seg4d2x595::Drv7seg4d2x595()
+{
+}
+
+void Drv7seg4d2x595::init_bb(uint32_t data_pin, uint32_t latch_pin, uint32_t clock_pin, \
+                             uint32_t ghosting_prevention_delay)
+{
+    this->variant = DRV7SEG4D2X595_VARIANT_BIT_BANGING;
+    this->data_pin = data_pin;
+    this->latch_pin = latch_pin;
+    this->clock_pin = clock_pin;
+    this->ghosting_prevention_delay = ghosting_prevention_delay;
+
+    pinMode(data_pin, OUTPUT);
+    pinMode(latch_pin, OUTPUT);
+    pinMode(clock_pin, OUTPUT);
+}
+
+void Drv7seg4d2x595::init_spi(uint32_t mosi_pin, uint32_t latch_pin, uint32_t sck_pin, \
+                              uint32_t ghosting_prevention_delay);
+{
+    this->variant = DRV7SEG4D2X595_VARIANT_SPI;
+    this->latch_pin = latch_pin;
+    this->ghosting_prevention_delay = ghosting_prevention_delay;
+
+    pinMode(latch_pin, OUTPUT);
+    SPI.begin(sck_pin, -1, mosi_pin, -1);
+}
+
+void Drv7seg4d2x595::init_spi(uint32_t latch_pin, uint32_t ghosting_prevention_delay)
+{
+    this->variant = DRV7SEG4D2X595_VARIANT_SPI;
+    this->latch_pin = latch_pin;
+    this->ghosting_prevention_delay = ghosting_prevention_delay;
+
+    pinMode(latch_pin, OUTPUT);
+    SPI.begin();
+}
+
+void Drv7seg4d2x595::shift_out(uint8_t ubyte, uint8_t lbyte)
+{
+    switch (this->variant) {
+        case DRV7SEG4D2X595_VARIANT_BIT_BANGING:
+            digitalWrite(latch_pin, 0);
+            shiftOut(data_pin, clock_pin, MSBFIRST, ubyte);
+            shiftOut(data_pin, clock_pin, MSBFIRST, lbyte);
+            digitalWrite(latch_pin, 1);
+
+            delay(this->ghosting_prevention_delay);
+            digitalWrite(latch_pin, 0);
+            shiftOut(data_pin, clock_pin, MSBFIRST, 0);
+            digitalWrite(latch_pin, 1);
+            break;
+
+        case DRV7SEG4D2X595_VARIANT_SPI:
+            digitalWrite(latch_pin, 0);
+            SPI.transfer(ubyte);
+            SPI.transfer(lbyte);
+            digitalWrite(latch_pin, 1);
+
+            delay(this->ghosting_prevention_delay);
+            digitalWrite(latch_pin, 0);
+            SPI.transfer(0);
+            digitalWrite(latch_pin, 1);
+            break;
+
+        default:
+            break;  // Do nothing and hail MISRA.
+    }
+}
---
+++ sketch/Billy/Drv7seg4d2x595.h
@@ -0,0 +1,60 @@
+/****************** DESCRIPTION *****************/
+
+/**
+ * Filename: Drv7seg4d2x595.h
+ * ----------------------------------------------------------------------------|---------------------------------------|
+ * Purpose: for ESP32
+ * ----------------------------------------------------------------------------|---------------------------------------|
+ * Notes:
+ */
+
+
+/************ PREPROCESSOR DIRECTIVES ***********/
+
+// Include guards.
+#ifndef DRV7SEG4D2X595_H
+#define DRV7SEG4D2X595_H
+
+
+/*--- Includes ---*/
+
+// Essential Arduino library.
+#include <Arduino.h>
+
+
+/*--- Misc ---*/
+
+#define DRV7SEG4D2X595_VARIANT_BIT_BANGING 0
+#define DRV7SEG4D2X595_VARIANT_SPI         1
+
+
+/****************** DATA TYPES ******************/
+
+class Drv7seg4d2x595 {
+    public:
+        Drv7seg4d2x595();
+
+        void init_bb(uint32_t data_pin, uint32_t latch_pin, uint32_t clock_pin, \
+                     uint32_t ghosting_prevention_delay);
+        void init_spi(uint32_t mosi_pin, uint32_t latch_pin, uint32_t sck_pin, \
+                      uint32_t ghosting_prevention_delay);
+        void init_spi(uint32_t latch_pin, uint32_t ghosting_prevention_delay);
+        void shift_out(uint8_t ubyte, uint8_t lbyte);
+
+    private:
+        uint32_t variant;
+        uint32_t data_pin;
+        uint32_t mosi_pin;
+        uint32_t latch_pin;
+        uint32_t clock_pin;
+        uint32_t sck_pin;
+        uint32_t ghosting_prevention_delay;
+};
+
+
+/*************** GLOBAL VARIABLES ***************/
+
+extern Drv7seg4d2x595 Daisy16;
+
+
+#endif  // Include guards.
