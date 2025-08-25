efd03bda88b3a5b59fb717037830359411274ce6
ErlingSigurdson <efimov-d-v@yandex.ru>
2025-08-25 17:46:44 +0300

    Create namespace for cmd local module.

--- sketch/Billy/cmd.h
+++ sketch/Billy/cmd.h
@@ -50,207 +50,215 @@ typedef struct set_config_params_t {
 
 /************** FUNCTION PROTOTYPES *************/
 
-/*--- Buffer contents check ---*/
+namespace cmd {
 
-// Look up for a valid prefix and for valid commands from a text commands list.
-int32_t cmd_check(char *buf, const char *prefix, const char *cmd_list[], uint32_t cmd_list_len);
+    /*--- Buffer contents check ---*/
 
-
-/*--- Auxiliary functions (helper functions, accessories) ---*/
-
-/* A generic accessory called by the other ones.
- * Prints a message over a UART and sends it over wireless connections.
- */
-void cmd_aux_output(const char *msg);
-
-/* A generic accessory called by the other ones.
- * Checks if the string contains anything aside from decimal digits and dots.
- */
-bool cmd_aux_has_decimal_only(const char *str);
-
-// Accessories for handler functions.
-void cmd_aux_set_output_digital(uint8_t pin, uint8_t state, const char *topic);
-void cmd_aux_set_output_PWM(uint8_t pin, uint32_t val, const char *topic);
-void cmd_aux_set_config(set_config_params_t* params);
-void cmd_aux_output_config(uint32_t addr, const char *topic);
+    // Look up for a valid prefix and for valid commands from a text commands list.
+    int32_t cmd_check(char *buf, const char *prefix, const char *cmd_list[], uint32_t cmd_list_len);
 
 
-/*--- Handler functions ---*/
+    namespace aux {
 
-/* Functions called in a response to received commands
- * and command processing errors.
- */
+        /*--- Auxiliary functions (helper functions, accessories) ---*/
 
-// Incoming byte string is too long.
-void cmd_handler_err_len();
+        /* A generic accessory called by the other ones.
+         * Prints a message over a UART and sends it over wireless connections.
+         */
+        void cmd_aux_output(const char *msg);
 
-// Invalid/absent command prefix.
-void cmd_handler_err_prefix();
+        /* A generic accessory called by the other ones.
+         * Checks if the string contains anything aside from decimal digits and dots.
+         */
+        bool cmd_aux_has_decimal_only(const char *str);
 
-// No valid command after a prefix.
-void cmd_handler_err_cmd();
+        // Accessories for handler functions.
+        void cmd_aux_set_output_digital(uint8_t pin, uint8_t state, const char *topic);
+        void cmd_aux_set_output_PWM(uint8_t pin, uint32_t val, const char *topic);
+        void cmd_aux_set_config(set_config_params_t* params);
+        void cmd_aux_output_config(uint32_t addr, const char *topic);
+    }
 
-// No valid value after a command which requires it.
-void cmd_handler_err_val();
 
-/* Command #1:
- * turn a two-state load ON or OFF. A main workhorse.
- * A prescribed state of the load is NOT stored in an inbuilt storage.
- */
-void cmd_handler_set_load_digital(char *cmd);
+    namespace handler {
 
-/* Command #2:
- * drive a load using PWM. Another main workhorse.
- * Valid values of a duty cycle are 0 to 255.
- * A prescribed duty cycle value is NOT stored in an inbuilt storage.
- */
-void cmd_handler_set_load_PWM(char *cmd);
+        /*--- Handler functions ---*/
 
-/* Command #3:
- * print and send to a client/master the current state of a digital (two-state) load.
- * The actual output is based on a return value of the digitalRead().
- */
-void cmd_handler_output_load_digital();
+        /* Functions called in a response to received commands
+         * and command processing errors.
+         */
 
-/* Command #4:
- * change an SSID of a Wi-Fi access point
- * stored in an inbuilt storage.
- */
-void cmd_handler_set_WiFi_SSID(char *cmd, bool *refresh_flag);
+        // Incoming byte string is too long.
+        void cmd_handler_err_len();
 
-/* Command #5:
- * print and send to a client/master an SSID of a Wi-Fi access point
- * stored in an inbuilt storage.
- */
-void cmd_handler_output_WiFi_SSID();
+        // Invalid/absent command prefix.
+        void cmd_handler_err_prefix();
 
-/* Command #6:
- * change a password used for connecting to a Wi-Fi access point
- * stored in an inbuilt storage.
- */
-void cmd_handler_set_WiFi_pswd(char *cmd, bool *refresh_flag);
+        // No valid command after a prefix.
+        void cmd_handler_err_cmd();
 
-/* Command #7:
- * turn periodical printing of a current RSSI value ON or OFF.
- */
-void cmd_handler_set_WiFi_RSSI_output_flag(char *cmd, bool *refresh_flag);
+        // No valid value after a command which requires it.
+        void cmd_handler_err_val();
 
-/* Command #8:
- * turn periodical attempts to reconnect to a Wi-Fi network ON or OFF.
- */
-void cmd_handler_set_WiFi_autoreconnect_flag(char *cmd, bool *refresh_flag);
+        /* Command #1:
+         * turn a two-state load ON or OFF. A main workhorse.
+         * A prescribed state of the load is NOT stored in an inbuilt storage.
+         */
+        void cmd_handler_set_load_digital(char *cmd);
 
-/* Command #9:
- * print and send to a client/master the device's IP in a Wi-Fi network.
- */
-void cmd_handler_output_local_server_IP();
+        /* Command #2:
+         * drive a load using PWM. Another main workhorse.
+         * Valid values of a duty cycle are 0 to 255.
+         * A prescribed duty cycle value is NOT stored in an inbuilt storage.
+         */
+        void cmd_handler_set_load_PWM(char *cmd);
 
-/* Command #10:
- * change a local TCP server port number
- * stored in an inbuilt storage.
- */
-void cmd_handler_set_local_server_port(char *cmd, bool *refresh_flag);
+        /* Command #3:
+         * print and send to a client/master the current state of a digital (two-state) load.
+         * The actual output is based on a return value of the digitalRead().
+         */
+        void cmd_handler_output_load_digital();
 
-/* Command #11:
- * print and send to a client/master a local TCP server port number
- * stored in an inbuilt storage.
- */
-void cmd_handler_output_local_server_port();
+        /* Command #4:
+         * change an SSID of a Wi-Fi access point
+         * stored in an inbuilt storage.
+         */
+        void cmd_handler_set_WiFi_SSID(char *cmd, bool *refresh_flag);
 
-/* Command #12:
- * set the IoT mode (attempts to connect to a remote server) ON or OFF.
- */
-void cmd_handler_set_IoT_flag(char *cmd, bool *refresh_flag);
+        /* Command #5:
+         * print and send to a client/master an SSID of a Wi-Fi access point
+         * stored in an inbuilt storage.
+         */
+        void cmd_handler_output_WiFi_SSID();
 
-/* Command #13:
- * change an IP address of a remote server
- * stored in an inbuilt storage.
- */
-void cmd_handler_set_IoT_server_IP(char *cmd, bool *refresh_flag);
+        /* Command #6:
+         * change a password used for connecting to a Wi-Fi access point
+         * stored in an inbuilt storage.
+         */
+        void cmd_handler_set_WiFi_pswd(char *cmd, bool *refresh_flag);
 
-/* Command #14:
- * print and send to a client/master an IP address of a remote server
- * stored in an inbuilt storage.
- */
-void cmd_handler_output_IoT_server_IP();
+        /* Command #7:
+         * turn periodical printing of a current RSSI value ON or OFF.
+         */
+        void cmd_handler_set_WiFi_RSSI_output_flag(char *cmd, bool *refresh_flag);
 
-/* Command #15:
- * change a port number presumably used by a remote server
- * stored in an inbuilt storage.
- */
-void cmd_handler_set_IoT_server_port(char *cmd, bool *refresh_flag);
+        /* Command #8:
+         * turn periodical attempts to reconnect to a Wi-Fi network ON or OFF.
+         */
+        void cmd_handler_set_WiFi_autoreconnect_flag(char *cmd, bool *refresh_flag);
 
-/* Command #16:
- * print and send to a client/master a port number presumably used by a remote server
- * stored in an inbuilt storage.
- */
-void cmd_handler_output_IoT_server_port();
+        /* Command #9:
+         * print and send to a client/master the device's IP in a Wi-Fi network.
+         */
+        void cmd_handler_output_local_server_IP();
 
-/* Command #17:
- * change a request message to be sent to a remote server
- * stored in an inbuilt storage.
- */
-void cmd_handler_set_IoT_req_msg(char *cmd, bool *refresh_flag);
+        /* Command #10:
+         * change a local TCP server port number
+         * stored in an inbuilt storage.
+         */
+        void cmd_handler_set_local_server_port(char *cmd, bool *refresh_flag);
 
-/* Command #18:
- * print and send to a client/master a request message to be sent to a remote server
- * stored in an inbuilt storage.
- */
-void cmd_handler_output_IoT_req_msg();
+        /* Command #11:
+         * print and send to a client/master a local TCP server port number
+         * stored in an inbuilt storage.
+         */
+        void cmd_handler_output_local_server_port();
 
-/* Command #19:
- * change an interval (in ms) of sending requests to a remote server
- * stored in an inbuilt storage.
- */
-void cmd_handler_set_IoT_req_period(char *cmd, bool *refresh_flag);
+        /* Command #12:
+         * set the IoT mode (attempts to connect to a remote server) ON or OFF.
+         */
+        void cmd_handler_set_IoT_flag(char *cmd, bool *refresh_flag);
 
-/* Command #20:
- * set Bluetooth Classic functionality ON or OFF.
- */
-void cmd_handler_set_BTClassic_flag(char *cmd,
-                                    void (*setup_BTClassic_ptr)(stored_configs_t *),
-                                    bool *refresh_flag);
+        /* Command #13:
+         * change an IP address of a remote server
+         * stored in an inbuilt storage.
+         */
+        void cmd_handler_set_IoT_server_IP(char *cmd, bool *refresh_flag);
 
-/* Command #21:
- * change a name of the ESP32 as a Bluetooth Classic slave device
- * stored in an inbuilt storage.
- */
-void cmd_handler_set_BTClassic_dev_name(char *cmd,
-                                        void (*setup_BTClassic_ptr)(stored_configs_t *),
-                                        bool *refresh_flag);
+        /* Command #14:
+         * print and send to a client/master an IP address of a remote server
+         * stored in an inbuilt storage.
+         */
+        void cmd_handler_output_IoT_server_IP();
 
-/* Command #22:
- * print and send to a client/master a name of the ESP32 as a Bluetooth Classic slave device
- * stored in an inbuilt storage.
- */
-void cmd_handler_output_BTClassic_dev_name();
+        /* Command #15:
+         * change a port number presumably used by a remote server
+         * stored in an inbuilt storage.
+         */
+        void cmd_handler_set_IoT_server_port(char *cmd, bool *refresh_flag);
 
-/* Command #23:
- * reset all wireless connections and restart a wireless connectivity.
- */
-void cmd_handler_all_conn_rst(bool (*setup_WiFi_ptr)(stored_configs_t *, uint32_t),
-                              void (*setup_BTClassic_ptr)(stored_configs_t *),
-                              stored_configs_t *stored_configs);
+        /* Command #16:
+         * print and send to a client/master a port number presumably used by a remote server
+         * stored in an inbuilt storage.
+         */
+        void cmd_handler_output_IoT_server_port();
 
-/* Command #24:
- * output a firmware version and specified pins information.
- */
-void cmd_handler_output_version();
+        /* Command #17:
+         * change a request message to be sent to a remote server
+         * stored in an inbuilt storage.
+         */
+        void cmd_handler_set_IoT_req_msg(char *cmd, bool *refresh_flag);
 
-/* Command #25:
- * output a given RGB color.
- */
-void cmd_handler_RGB_output_color(char *cmd);
+        /* Command #18:
+         * print and send to a client/master a request message to be sent to a remote server
+         * stored in an inbuilt storage.
+         */
+        void cmd_handler_output_IoT_req_msg();
 
-/* Command #26:
- * turn RGB output ON.
- */
-void cmd_handler_RGB_output_on();
+        /* Command #19:
+         * change an interval (in ms) of sending requests to a remote server
+         * stored in an inbuilt storage.
+         */
+        void cmd_handler_set_IoT_req_period(char *cmd, bool *refresh_flag);
 
-/* Command #27:
- * turn RGB output OFF.
- */
-void cmd_handler_RGB_output_off();
+        /* Command #20:
+         * set Bluetooth Classic functionality ON or OFF.
+         */
+        void cmd_handler_set_BTClassic_flag(char *cmd,
+                                            void (*setup_BTClassic_ptr)(stored_configs_t *),
+                                            bool *refresh_flag);
+
+        /* Command #21:
+         * change a name of the ESP32 as a Bluetooth Classic slave device
+         * stored in an inbuilt storage.
+         */
+        void cmd_handler_set_BTClassic_dev_name(char *cmd,
+                                                void (*setup_BTClassic_ptr)(stored_configs_t *),
+                                                bool *refresh_flag);
+
+        /* Command #22:
+         * print and send to a client/master a name of the ESP32 as a Bluetooth Classic slave device
+         * stored in an inbuilt storage.
+         */
+        void cmd_handler_output_BTClassic_dev_name();
+
+        /* Command #23:
+         * reset all wireless connections and restart a wireless connectivity.
+         */
+        void cmd_handler_all_conn_rst(bool (*setup_WiFi_ptr)(stored_configs_t *, uint32_t),
+                                      void (*setup_BTClassic_ptr)(stored_configs_t *),
+                                      stored_configs_t *stored_configs);
+
+        /* Command #24:
+         * output a firmware version and specified pins information.
+         */
+        void cmd_handler_output_version();
+
+        /* Command #25:
+         * output a given RGB color.
+         */
+        void cmd_handler_RGB_output_color(char *cmd);
+
+        /* Command #26:
+         * turn RGB output ON.
+         */
+        void cmd_handler_RGB_output_on();
+
+        /* Command #27:
+         * turn RGB output OFF.
+         */
+        void cmd_handler_RGB_output_off();
+    }
 
 
 #endif  // Include guards.
