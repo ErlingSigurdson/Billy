3b36d7ee1f481492b6a73e54cd203699025a12c0
Erling Sigurdson <efimov-d-v@yandex.ru>
2025-09-08 03:29:57 +0300

    Update a function name in a merged code.

--- sketch/Billy/Billy.ino
+++ sketch/Billy/Billy.ino
@@ -554,7 +554,9 @@ void loop()
         static bool digital_load_toggle_flag = 0;
         if (SimpleCounter.minutes % 5 == 0 && SimpleCounter.seconds == 0) {  // Toggles every five minutes.
             if (digital_load_toggle_flag) {
-                cmd_handler_set_load_digital(CMD_PREFIX CMD_1 "TOGGLE");
+                cmd::handler::set_load_digital(CMD_PREFIX CMD_1 "TOGGLE",
+                                               DIGITAL_OUTPUT_PIN,
+                                               DIGITAL_OUTPUT_ACTIVE_STATE);
                 digital_load_toggle_flag = 0;
             }
         } else {
